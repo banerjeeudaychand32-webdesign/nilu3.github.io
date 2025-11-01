9ac3faffeacc6c8dda6dbed2123e14ad83363dd2
nilors banerjee <banerjeeudaychand32@gmail.com>
2025-10-31 23:20:46 -0700

    index

---
+++ script.js
@@ -0,0 +1,15 @@
+function showResponse() {
+    let name = document.getElementById("name").value;
+    let msg = document.getElementById("msg").value;
+
+    if (!name || !msg) {
+        document.getElementById("response").innerText = "Please fill all fields!";
+        return;
+    }
+
+    document.getElementById("response").innerText =
+        "Hi " + name + ", your message is: " + msg;
+
+    document.getElementById("name").value = "";
+    document.getElementById("msg").value = "";
+}
