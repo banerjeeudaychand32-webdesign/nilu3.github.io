5e7c330a2a98298c655150e5bbba34ff2c1ec0b5
nilors banerjee <banerjeeudaychand32@gmail.com>
2025-11-01 00:07:37 -0700

    lo

--- script.js
+++ script.js
@@ -8,9 +8,23 @@ function showResponse() {
     }
 
     document.getElementById("response").innerText =
-        
-    "Hi " + name + ", your message is: " + msg;
+        "Hi " + name + ", your message is: " + msg;
 
+    document.getElementById("name").value = "";
+    document.getElementById("msg").value = "";
+}
+
+function showResponse() {
+    let name = document.getElementById("name").value;
+    let msg = document.getElementById("msg").value;
+
+    if (!name || !msg) {
+        document.getElementById("response").innerText = "Please fill all fields!";
+        return;
+    }
+
+    document.getElementById("response").innerText =
+        "Hi " + name + ", your message is: " + msg;
 
     document.getElementById("name").value = "";
     document.getElementById("msg").value = "";
